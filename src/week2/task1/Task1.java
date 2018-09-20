29ba37acb8a720d4977c493f54db5bc2c464a37c
DinhNguyen <nguyendinh180299@gmail.com>
2018-09-20 22:26:48 +0700

    week2

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,25 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
+        if(a == b)
+            return a;
+        if(a > b)
+            return gcd(a-b, b);
+        if(a < b)
+            return gcd(a, b-a);
         return 0;
     }
 
     public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
+        if(n == 0 || n ==  1)
+            return 1;
+        if(n >= 2)
+            return fibonacci(n-1)+ fibonacci(n-2);
         return 0;
+    }
+    
+    public static void main(String[] args){
+        System.out.println(gcd(4, 2));
+        System.out.println(fibonacci(5));
     }
 }
