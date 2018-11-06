5f778f3ff658823c69b05a3eb25987a21c6d9811
DinhNguyen <nguyendinh180299@gmail.com>
2018-11-06 15:19:50 +0700

    week9

--- src/week7/task1/ExpressionTest.java
+++ src/week7/task1/ExpressionTest.java
@@ -20,7 +20,7 @@ public class ExpressionTest {
             System.out.println(div1.evaluate());
         }
         catch(ArithmeticException e){
-            System.err.println(e);
+            System.out.println(e);
         }
 
     }
--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,4 +1,97 @@
 package week9;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Utils {
+    private static final String path = "D:\\pathname.txt";
+    private static final String path2 = "D:\\pathname2.txt";
+
+    public static String readContentFromFile(String path){
+        String content = null;
+        try {
+            content = new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    public static void writeContentToFile_B(String path){
+        FileWriter f = null;
+        BufferedWriter b = null;
+        try{
+            String data = "\nGhi de du lieu len du lieu cu";
+            File file = new File(path);
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            f = new FileWriter(file.getAbsoluteFile());
+            b = new BufferedWriter(f);
+            b.write(data);
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                if(b != null)
+                    b.close();
+                if(f != null)
+                    f.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void writeContentToFile_C(String path){
+        FileWriter f = null;
+        BufferedWriter b = null;
+        try{
+            String data = "\nHoc tai Dai hoc Cong nghe";
+            File file = new File(path);
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            f = new FileWriter(file.getAbsoluteFile(), true);
+            b = new BufferedWriter(f);
+            b.write(data);
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                if(b != null)
+                    b.close();
+                if(f != null)
+                    f.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static File findFileByName(String folderPath, String fileName){
+        return null;
+    }
+
+    public static void main(String[] args) {
+        writeContentToFile_B(path2);
+        System.out.println(readContentFromFile(path2));
+        writeContentToFile_C(path);
+        System.out.println(readContentFromFile(path));
+
+    }
 }
