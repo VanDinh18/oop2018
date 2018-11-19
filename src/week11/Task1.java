0674c50cc73a1b1a98b8a7facce9574d27b679fc
DinhNguyen <nguyendinh180299@gmail.com>
2018-11-20 00:53:19 +0700

    week11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -2,20 +2,38 @@ package week11;
 
 import java.util.Arrays;
 
+
 public class Task1 {
-    public static <T> void printArray(T[] inputArray){
-        Arrays.sort(inputArray);
-        for(T s : inputArray){
+    public static <T extends Comparable> void sortArray(T[] a){
+        //Arrays.sort(a);
+        int n = a.length;
+        boolean b = true;
+        while (b)
+        {
+            b = false;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (a[i].compareTo(a[i + 1]) > 0)
+                {
+                    T temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    b = true;
+                }
+            }
+        }
+        for(T s : a){
             System.out.println(s);
         }
+
     }
 
 
     public static void main(String[] args) {
         Integer[] int_ = {1, 4, 5, 2, 9, 0};
-        printArray(int_);
+        sortArray(int_);
 
-        String [] char_ = {"a", "d", "k", "e", "c", "b"};
-        printArray(char_);
+        String [] string_ = {"a", "d", "k", "e", "c", "b"};
+        sortArray(string_);
     }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,7 +1,25 @@
 package week11;
 
 import java.util.Arrays;
+import java.util.Iterator;
+
 
 public class Task2 {
-    
+    public static  <T extends Comparable> T maxArray(T[] a){
+        T max = a[0];
+        for(T s : a){
+            if(s.compareTo(max) > 0){
+                max = s;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Integer [] a = {1, 9, 4, 5};
+        Integer i = maxArray(a);
+        Character [] a_ = {'a', 'd', 'c', 'b'};
+        Character c = maxArray(a_);
+        System.out.println(i + " " + c);
+    }
 }
