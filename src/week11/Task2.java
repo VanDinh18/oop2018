1562a40041791e59fb4c60ba109c35f76aa4a6d9
DinhNguyen <nguyendinh180299@gmail.com>
2018-11-21 19:44:27 +0700

    week11

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,5 +1,6 @@
 package week11;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -14,12 +15,31 @@ public class Task2 {
         }
         return max;
     }
-
-    public static void main(String[] args) {
+    public static  <T extends Comparable> T _maxArray(ArrayList a){
+        T max = (T) a.get(0);
+        for(int i=0; i<a.size(); i++){
+            if(max.compareTo(a.get(i)) < 0){
+                max = (T)a.get(i);
+            }
+        }
+        return max;
+    }
+    public static  void main(String[] args) {
         Integer [] a = {1, 9, 4, 5};
         Integer i = maxArray(a);
         Character [] a_ = {'a', 'd', 'c', 'b'};
         Character c = maxArray(a_);
-        System.out.println(i + " " + c);
+        //System.out.println(i + " " + c);
+
+
+        ArrayList<Integer> arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(5);
+        arrayList.add(9);
+        arrayList.add(6);
+
+        Integer maxI = _maxArray(arrayList);
+        System.out.println(maxI);
     }
+
 }
